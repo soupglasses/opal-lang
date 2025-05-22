4a853aec697ef0a73f01b5c29478779b31bd48e6
FalseViking <sorenbks@gmail.com>
2025-05-22 16:54:59 +0200

    LexerASI test integrated into lexer_test

--- test/opal/lexer_test.exs
+++ test/opal/lexer_test.exs
@@ -57,4 +57,33 @@ defmodule Opal.LexerTest do
       assert {:error, _} = Lexer.tokenize("1z")
     end
   end
+
+  describe "Automatic Semicolon Insertion" do
+    test "ASI does not insert semicolon after certain tokens followed by newline" do
+      # Using operators that shouldn't trigger ASI
+      code = """
+      x +
+      y
+      """
+      {:ok, tokens} = Lexer.tokenize(code)
+
+      refute Enum.any?(Enum.chunk_every(tokens, 2, 1, :discard), fn
+        [ {'+', _}, ~c";" ] -> false
+        _ -> false
+      end)
+    end
+    test "ASI Does not trigger on newline mid expression" do
+      # Using operators that shouldn't trigger ASI
+      code = """
+      x
+      + y
+      """
+      {:ok, tokens} = Lexer.tokenize(code)
+
+      refute Enum.any?(Enum.chunk_every(tokens, 2, 1, :discard), fn
+        [ {'+', _}, ~c";" ] -> false
+        _ -> false
+      end)
+    end
+  end
 end
